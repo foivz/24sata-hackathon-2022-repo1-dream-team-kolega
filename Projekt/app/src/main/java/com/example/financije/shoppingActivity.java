71c33cd0bd52eb1a7655aaf8473c5a22f4252e77
josipsavoric <josip.savoric@gmail.com>
2022-04-02 14:01:50 +0200

    yeet

--- Projekt/app/src/main/java/com/example/financije/shoppingActivity.java
+++ Projekt/app/src/main/java/com/example/financije/shoppingActivity.java
@@ -25,10 +25,7 @@ public class shoppingActivity extends AppCompatActivity {
         shoopingRec.setLayoutManager(new GridLayoutManager(this,2));
 
         ArrayList<Baza> produkt = new ArrayList<>();
-        produkt.add(new Baza("kobasa","50","https://ferbezar.com/wp-content/uploads/2016/11/slavonska-kobasica1.jpg"));
-        produkt.add(new Baza("Hren","20","https://podravkaiovariations.azureedge.net/05aa6e54-8f62-11ea-916b-fefaf5a5a600/v/9ed607bc-4c1b-11ea-9bb0-92f307bc0925/450x600-9ed6ed4e-4c1b-11ea-9140-92f307bc0925.png"));
-        produkt.add(new Baza("Paradajs","10","https://alternativa-za-vas.com/images/uploads/paradajz3.jpg"));
-
+        produkt.add(new Baza())
         adapter.setProdukt(produkt);
     }
 }
