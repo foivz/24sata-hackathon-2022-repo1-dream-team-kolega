0f2dfa8f2b6b78b90c6af95930b66fdd691e8a2d
josipsavoric <josip.savoric@gmail.com>
2022-04-02 17:53:32 +0200

    Nek nam bog pomogne

--- Projekt/app/src/main/java/com/example/financije/LoadingActivity.java
+++ Projekt/app/src/main/java/com/example/financije/LoadingActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -12,6 +13,16 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Stvar();
+            }
+        }, 1000);
+
+    }
+    public void Stvar(){
+
         Intent intent;
         if (isLogedIn==false) intent=new Intent(LoadingActivity.this,LoginActivity.class);
         else intent=new Intent(LoadingActivity.this,MainActivity.class);
